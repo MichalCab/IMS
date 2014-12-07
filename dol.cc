2ed27c14652ca257531197bf72ed34af9d634c93
fossilmfc <fossilmfc@gmail.com>
2014-12-07 14:20:22 +0100

    kozmeticke upravy, opravy pocitadiel + prvy vypis zarobku na trasu

--- dol.cc
+++ dol.cc
@@ -1,53 +1,48 @@
 #include <string>
 #include <sstream>
-#include <iterator>
-#include <iostream>     // std::cout
-//#include <algorithm>    // std::find
-#include <vector>       // std::vector
 
 #include "simlib.h"
 
-// time of simulation:  T_KON - stop
-const double T_KON = 365*24;   // hodin
-
-// definice konstant
-const double T_PLNENI_KOMORY  = 14/60;   // doba plnění komory
-const double T_VJEZD_LODI_DO_KOMORY  = 10/60;   // doba obsluhy lodě
-const double T_OBSLUHY = T_PLNENI_KOMORY + T_VJEZD_LODI_DO_KOMORY;
-const double T_PRICH = 1; // příchod lodi
-const double T_RYCHLOST_LODI = 0.3;
-
-const int N_KILOMETRY = 150;
-
-const int KOMORY[] = {40, 60,  90, 130}; // na jakych kilometrech jsou komory
-const int N_KOMORY = (sizeof(KOMORY) / sizeof(const int));
-
-// deklarace globalnich objektu
-Facility Komora[N_KOMORY];           // komora
-Facility Kilometr[N_KILOMETRY];
-
-unsigned CelkovyPocLodi = 0; // celkový počet lodí
-unsigned PocLodiNaTrase = 0;    // aktuální počet lodí na trase
-unsigned CelkovyPocetKilometru = 0;
-
-Histogram Tabulka ("Čekací doby lodi",0,0.15,20);
-
 using namespace std;
 
-class Lod : public Process {  // 
-  double Vyjezd;               // čas vjezdu lodě do systému
+// definice konstant
+const double T_KON = 365*24;    // kolik simulovanych hodin
+
+const double T_PLNENI_KOMORY = 14/60;                   // doba plnění komory
+const double T_VJEZD_A_VYJEZD_LODI_DO_Z_KOMORY  = 20/60;    // doba obsluhy lodě
+const double T_OBSLUHY_KOMORY = T_PLNENI_KOMORY + T_VJEZD_A_VYJEZD_LODI_DO_Z_KOMORY;
+const double T_RYCHLOST_LODI = 0.07;                    // doba za kterou lod projde kilometr
+const int N_KILOMETRY = 150;                            // celkem kilometru na trase
+
+const int KOMORY[] = {40, 60,  90, 130};                // na jakych kilometrech jsou komory
+const int N_KOMORY = (sizeof(KOMORY) / sizeof(int));    // pocet komor
+
+
+// deklarace globalnich objektu
+Facility Komora[N_KOMORY];                  // komory
+Facility Kilometr[N_KILOMETRY];             // kilometry
+
+double T_PRICH = 1;                         // příchod lodi
+double CENA_KONTEJNR = 50;                  // priblizna cena za naklad a vyklad jednoho kontejneru
+double L_KAPACITA = 470;                    // kapacita lodi v poctu kontejneru
+double L_MIN_KAPACITA = 200;                // minimalni zaplnena kapacita pro vypluti lode
+
+unsigned CelkovyPocLodi = 0;                // celkový počet lodí
+unsigned PocLodiNaTrase = 0;                // aktuální počet lodí na trase
+unsigned CelkovyPocetKilometru = 0;
+
+class Lod : public Process {
   unsigned int UjetychKilometru;
   int LodId;
-  void Behavior() // popis chování lodi
+  bool LodVyjela;
+  void Behavior()       // popis chování lodi
   {
-    Vyjezd = Time;
-    PocLodiNaTrase++;
-    CelkovyPocLodi++;
-	LodId = CelkovyPocLodi;
-    while (UjetychKilometru < N_KILOMETRY) // dokud má kam jet
+    LodVyjela = false;
+    UjetychKilometru = 0;
+    while (UjetychKilometru < N_KILOMETRY)      // dokud má kam jet
     {
         int poziceKomory = -1;
-        for (int i = 0; i < N_KOMORY; i++)
+        for (int i = 0; i < N_KOMORY; i++)      // Cyklus kontrolujici, zda jde lod do plavebni komory
         {
             if (KOMORY[i] == UjetychKilometru)
             {
@@ -55,48 +50,59 @@ class Lod : public Process {  //
                 break;
             }
         }
-        if (poziceKomory != -1)// pokud je na daném kilometru komora, vjede do ní
+        if (poziceKomory != -1)                 // pokud je na daném kilometru komora, vjede do ní
         {
-            Seize(Komora[poziceKomory]);
-            Wait(T_OBSLUHY);
-            Release(Komora[poziceKomory]);
+            Seize(Komora[poziceKomory]);        // Lod obsazuje plavebni komoru
+            Wait(T_OBSLUHY_KOMORY);             // Lod ceka na dokonceni obsluhy plavebni komory aby mohla plout dal
+            Release(Komora[poziceKomory]);      // Opousti plavebni komoru
         }
-        Seize(Kilometr[UjetychKilometru]);
-        Wait(T_RYCHLOST_LODI);
-        Release(Kilometr[UjetychKilometru]);
-		UjetychKilometru++;
-		CelkovyPocetKilometru++;
+        Seize(Kilometr[UjetychKilometru]);      // Lod pluje, obsazuje dalsi kilometr
+        if (LodVyjela == false)                 // Tohle je za potrebi v pripade, ze lod chce vyplout priliz brzo, kdy neni ani prvni kilometr volny
+        {
+            PocLodiNaTrase++;                   // Na trase se nachazi o lod navic
+            CelkovyPocLodi++;                   // Pribiva dalsi lod za celkovy cas simulace
+            LodId = CelkovyPocLodi;             // Identifikace cisla lodi na trase
+            LodVyjela = true;                   // Lod vyjela
+        }
+        Wait(T_RYCHLOST_LODI);                  // Lod pluje obsazeny kilometr
+        Release(Kilometr[UjetychKilometru]);    // Lod opousti obsazeny kilometr, uvolnuje
+        
+        UjetychKilometru++;                     // Lod proplula dalsi kilometr
+        CelkovyPocetKilometru++;                // Na tomhle useku bylo lodi najeto dalsi kilometr
     }
-    PocLodiNaTrase--; // loď odplouvá ze systéme
-    Tabulka(Time - Vyjezd);    // záznam o výjezdu
+    PocLodiNaTrase--;                           // loď konci svou plavbu, vyklada svuj naklad
   }
 };
 
-class Generator : public Event {        //
-  void Behavior() {                     //
-    (new Lod)->Activate();         //
+class Generator : public Event {
+  void Behavior() {
+    (new Lod)->Activate();
     Activate(Time + Exponential(T_PRICH));
   }
 };
 
 int main() {
-    //SetOutput("dol.out");    // output redirection
-    for(int i = 0; i < N_KOMORY; i++)
-    {
-        std::stringstream nazevKomory;
-        nazevKomory << "Komora " << i;
-        std::string str = nazevKomory.str();
-        Komora[i].SetName(str.c_str());
-    }
+    T_PRICH = 5;
+    // for(int i = 0; i < N_KOMORY; i++)
+    // {
+        // std::stringstream nazevKomory;
+        // nazevKomory << "Komora " << i;
+        // std::string str = nazevKomory.str();
+        // Komora[i].SetName(str.c_str());
+    // }
     Init(0, T_KON);         // initialize, non-zero initial time
-    Tabulka.Clear();            // we start at non-zero time
     (new Generator)->Activate();        // generator lodí
     Print(" DOL --- simulace průjezdu nákladních lodí\n");
     Run();                      // simulation
     Print("\n Systémem nestihlo projet %u lodí, které by chtěly \n\n", PocLodiNaTrase);
     Print("\n Systémem prošlo %u lodí \n\n", CelkovyPocLodi);
     Print("\n Najeto kilometru celkem %u \n\n", CelkovyPocetKilometru);
-    Tabulka.Output();
+    int zarobek = 0;
+    for (int i = 0; i < CelkovyPocLodi; i++) {
+        int kontejneru = L_MIN_KAPACITA + Exponential(L_KAPACITA - L_MIN_KAPACITA);
+        zarobek += kontejneru * CENA_KONTEJNR;
+    }
+    Print("\n Trasa vydelala priblizne %d \n\n", zarobek);
     SIMLIB_statistics.Output(); // print run statistics
 }
 
