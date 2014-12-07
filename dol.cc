c3e36cb367907c64db6f261b6691f4260bab20ff
Michal Cáb <majklcab@gmail.com>
2014-12-07 10:49:14 +0000

    aplha version

--- dol.cc
+++ dol.cc
@@ -1,7 +1,99 @@
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <iostream>     // std::cout
+//#include <algorithm>    // std::find
+#include <vector>       // std::vector
+
 #include "simlib.h"
 
-int main()
-{
-	Print("a\n");
-	return 0;
+// time of simulation:  T_KON - stop
+const double T_KON = 365*24;   // hodin
+
+// definice konstant
+const double T_PLNENI_KOMORY  = 14/60;   // doba plnění komory
+const double T_VJEZD_LODI_DO_KOMORY  = 10/60;   // doba obsluhy lodě
+const double T_OBSLUHY = T_PLNENI_KOMORY + T_VJEZD_LODI_DO_KOMORY;
+const double T_PRICH = 1; // příchod lodi
+const double T_RYCHLOST_LODI = 18*60;
+
+const int N_KOMORY = 9;
+const int N_KILOMETRY = 150;
+
+const int KOMORY[] = {40, 90, 130}; // na jakych kilometrech jsou komory
+
+// deklarace globalnich objektu
+Facility Komora[N_KOMORY];           // komora
+Facility Kilometr[N_KILOMETRY];
+
+unsigned CelkovyPocLodi = 0; // celkový počet lodí
+unsigned PocLodiNaTrase = 0;    // aktuální počet lodí na trase
+
+Histogram Tabulka ("Českací doby lodi",0,0.15,20);
+
+using namespace std;
+
+class Lod : public Process {  // 
+  double Vyjezd;               // čas vjezdu lodě do systému
+  unsigned int UjetychKilometru;
+  unsigned int Komor;
+  void Behavior() // popis chování lodi
+  {
+    Vyjezd = Time;
+    PocLodiNaTrase++;
+    CelkovyPocLodi++;
+    while (UjetychKilometru < N_KILOMETRY) // dokud má kam jet
+    {
+        bool found = false;
+        for (int i = 0; i < sizeof(KOMORY) * sizeof(int); i++)
+        {
+            if (KOMORY[i] == UjetychKilometru)
+            {
+                found = true;
+                break;
+            }
+        }
+        if (found)// pokud je na daném kilometru komora, vjede do ní
+        {
+            Seize(Komora[Komor]);
+            Wait(T_OBSLUHY);
+            Release(Komora[Komor]);
+        }
+        Seize(Kilometr[UjetychKilometru]);
+        Wait(T_RYCHLOST_LODI);
+        UjetychKilometru++;
+        Release(Kilometr[UjetychKilometru-1]);
+    }
+    PocLodiNaTrase--; // loď odplouvá ze systéme
+    Tabulka(Time - Vyjezd);    // záznam o výjezdu
+  }
+};
+
+class Generator : public Event {        //
+  void Behavior() {                     //
+    (new Lod)->Activate();         //
+    Activate(Time + Exponential(T_PRICH));
+  }
+};
+
+int main() {
+    //SetOutput("dol.out");    // output redirection
+    for(int i = 0; i < N_KOMORY; i++)
+    {
+        std::stringstream nazevKomory;
+        nazevKomory << "Komora " << i;
+        std::string str = nazevKomory.str();
+        Komora[i].SetName(str.c_str());
+    }
+    Init(0, T_KON);         // initialize, non-zero initial time
+    Tabulka.Clear();            // we start at non-zero time
+    (new Generator)->Activate();        // generator lodí
+    Print(" DOL --- simulace průjezdu nákladních lodí\n");
+    Run();                      // simulation
+    Print("\n Systémem nestihlo projet %u lodí, které by chtěly \n\n", PocLodiNaTrase);
+    Print("\n Systémem prošlo %u lodí \n\n", CelkovyPocLodi);
+    Tabulka.Output();
+    SIMLIB_statistics.Output(); // print run statistics
 }
+
+//
